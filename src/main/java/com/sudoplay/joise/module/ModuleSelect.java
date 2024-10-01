a5bbd4ac6db2e2c2b175e69b5652a0f93c0e16dd
bergice <kiste.andre@gmail.com>
2024-10-02 01:37:09 +1300

    Ad more constructors

--- src/main/java/com/sudoplay/joise/module/ModuleSelect.java
+++ src/main/java/com/sudoplay/joise/module/ModuleSelect.java
@@ -124,6 +124,15 @@ public class ModuleSelect extends
     setFalloff(falloff);
   }
 
+  public ModuleSelect(String id, double lowSource, Module highSource, Module controlSource, double threshold, double falloff) {
+    this(id);
+    setLowSource(lowSource);
+    setHighSource(highSource);
+    setControlSource(controlSource);
+    setThreshold(threshold);
+    setFalloff(falloff);
+  }
+
     public void setLowSource(double source) {
     this.low.set(source);
   }
